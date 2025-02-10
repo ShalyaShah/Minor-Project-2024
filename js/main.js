795adc26cf742821850ec4624a01984204b19d5a
Shalya Shah <shalyashah2802@gmail.com>
2025-02-10 17:43:27 +0530

    Minor Front End Changes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const one_way_trip=document.getElementById('one_way_trip');
+const round_trip=document.getElementById('round_trip');
+const return_date=document.getElementById('return_date');
+
+one_way_trip.addEventListener('click',function(){
+    return_date.style.display="none";
+})
+round_trip.addEventListener('click',function(){
+    return_date.style.display="block";
+})
